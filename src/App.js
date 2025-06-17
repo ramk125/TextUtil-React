1b2bc2628097eaff6ca27d1151fdf53381a658da
Ram Kumar <ramk060551@gmail.com>
2025-06-17 19:09:44 +0530

    final deploy changes

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type
-    });
-    setTimeout(() => {
-      setAlert(null);
-    }, 1500);
+    setAlert({ msg: message, type: type });
+    setTimeout(() => setAlert(null), 1500);
   };
 
   const toggleMode = () => {
@@ -43,8 +39,8 @@ function App() {
       <Alert alert={alert} />
       <div className="container my-3">
         <Routes>
-          <Route exact path="/about" element={<About mode={mode} />} />
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
+          <Route path="/about" element={<About mode={mode} />} />
+          <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
         </Routes>
       </div>
     </Router>
